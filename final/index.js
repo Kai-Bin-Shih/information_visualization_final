422e931e9597bc1021b4e64d8bd74cda2b6157e0
Kai-Bin-Shih <71625705+Kai-Bin-Shih@users.noreply.github.com>
2022-06-11 23:40:16 +0800

    Add files via upload

--- final/index.js
+++ final/index.js
@@ -47,15 +47,15 @@ function updateAll() {
         
             // for test----------------------------------
             for (i = features.length - 1 ; i >= 0 ; i--){
-                features[i].properties.NUMBER = 100;
+                features[i].properties.NUMBER = 50;
             }
-        
-            let path = d3.geoPath().projection(
-                d3.geoMercator()
-                    .center([121, 24])
-                    .scale(height * 12)
-                    .translate([width / 2, height / 2.5])
-            );
+            
+            var projection = d3.geoMercator()
+                                .center([121, 24])
+                                .scale(height * 12)
+                                .translate([width / 2, height / 2.5])
+
+            let path = d3.geoPath().projection(projection);
             
             // svg
             let svg = d3.select("body")
@@ -63,7 +63,11 @@ function updateAll() {
                     .append("svg")
                     .attr("width", width)
                     .attr("height", height)
-                    .selectAll('path')
+
+            var layer1 = svg.append("g")
+            var layer2 = svg.append("g")
+
+            var mapGraph = layer1.selectAll('path')
                     .data(features)
                     .enter()
                     .append('path')
@@ -77,7 +81,56 @@ function updateAll() {
                     .on('mouseout', function(){
                         d3.select(this).attr("fill-opacity", 1);
                     })
-        
+                    
+
+            var pointGraph = layer2.selectAll("circle")
+                    .data(getSelectedData(begin, end))
+                    .enter()
+                    .append("circle")
+                    .attr("cx", function(d) {
+                        return projection([d.EpicenterLon, d.EpicenterLat])[0];
+                    })
+                    .attr("cy", function(d) {
+                        return projection([d.EpicenterLon, d.EpicenterLat])[1];
+                    }) 
+                    .style("fill", function(d) {
+                        if(parseFloat(d.MagnitudeValue) <= 3) {
+                            return "#55AA00";
+                        } else if (parseFloat(d.MagnitudeValue) > 3 && parseFloat(d.MagnitudeValue) <= 4) {
+                            return "#227700"; 
+                        } else if (parseFloat(d.MagnitudeValue) > 4 && parseFloat(d.MagnitudeValue) <= 5) {
+                            return "#FFCC22";
+                        } else if (parseFloat(d.MagnitudeValue) > 5 && parseFloat(d.MagnitudeValue) <= 6) {
+                            return "#EE7700";
+                        } else {
+                            return "#CC0000";
+                        }
+                    })
+                    .attr("r", 2)
+            
+            var pointGroup = ["<=3", "3~4", "4~5", "5~6", ">6"]
+            var pointColorGroup = ["#55AA00", "#227700", "#FFCC22", "#EE7700", "#CC0000"]
+
+            var pointLabel = layer2.selectAll("rect")
+                    .data(pointGroup)
+                    .enter()
+                    .append("circle")
+                    .attr("cx", 80)
+                    .attr("cy", function(d,i){ return 10 + i * 25})
+                    .attr("r", 7)
+                    .style("fill", function(d,i){ return pointColorGroup[i]})
+
+            var pointText = layer2.selectAll("text")
+                    .data(pointGroup)
+                    .enter()
+                    .append("text")
+                    .attr("x", 80 + 20*.8)
+                    .attr("y", function(d,i){ return 11 + i * 25})
+                    .style("fill", function(d,i){ return pointColorGroup[i]})
+                    .text(function(d){ return d})
+                    .attr("text-anchor", "left")
+                    .style("alignment-baseline", "middle")
+                    
             //建立顏色漸變
             /* var maxValue = Math.max(...num)
             var minValue = Math.min(...num) */
@@ -91,8 +144,8 @@ function updateAll() {
             /* var a = d3.rgb(0,255,255);	
             var b = d3.rgb(0,0,255); */
         
-            var a = "#98F898"
-            var b = "#006400"
+            var a = "#AAAAAA"
+            var b = "#444444"
             
             //顏色插值函式
             var computeColor = d3.interpolate(a,b);
@@ -105,7 +158,7 @@ function updateAll() {
                 colorValue[name] = value
             }
             
-            svg.attr("fill", function(d, i){
+            mapGraph.attr("fill", function(d, i){
                 //console.log(d)
                 var t = linear( colorValue[d.properties.COUNTYNAME] ); 
                 var color = computeColor(t);
@@ -119,7 +172,7 @@ function updateAll() {
             let beginDate = new Date(begin);
             let endDate = new Date(end);
 
-            dataList = [1]
+            dataList = []
             var currentDate = ""
             for(index in data) {
                 var d = data[index].Year + "-" + data[index].Month + "-" + data[index].Day;
@@ -136,6 +189,17 @@ function updateAll() {
     
         
     })
+}
+
+function autoUpdate(begin, end) {
+    let beginDate = new Date(begin)
+    
+}
+
+function addOneDay(date, days){
+    var res = new Date(date);
+    res.setDate(res.getDate() + days);
+    return res;
 }
 
 
